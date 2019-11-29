18e261d3bd81eb4b30dd529ff78010d4f74943e5
quanzongwei <552114141@qq.com>
2019-11-29 13:31:13 +0800

    [算法]: 头条 两数相加-引用复制问题

--- src/main/java/com/qzw/demo/java/引用复制传递.java
+++ src/main/java/com/qzw/demo/java/引用复制传递.java
@@ -13,14 +13,14 @@ public class 引用复制传递 {
     public void testReference() {
         ListNode node = new ListNode(0);
 
-        foo(node);
+        function(node);
         // 引用传递对应的是两个引用, 所以这里的node的引用不会在foo中被重新指向新的内存区域
         // 如果想达到这种效果, 可以加一个容器, 然后修改容器中的值
         // 值传递对应的是两个值
         Assert.assertEquals(node.val, 0);
     }
 
-    private void foo(ListNode node) {
+    private void function(ListNode node) {
         node = new ListNode(1);
         System.out.println();
     }
